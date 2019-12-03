742a91c121d1df37c74ced7fe20bfbec1f047d7e
Successful-Minecraft <55243193+Successful-Minecraft@users.noreply.github.com>
2019-12-03 01:54:57 +0000

    Update tempory.java

--- tempory.java
+++ tempory.java
@@ -292,3 +292,345 @@ public class Pie extends Application {
     }
 
 }
+
+
+
+import java.awt.datatransfer.MimeTypeParseException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MiniInterpreter {
+    public ArrayList<String>    lines;
+    public ArrayList<Integer>   wholenumbers;
+    public ArrayList<String>    names;
+    public int                  index;
+
+    public MiniInterpreter(){
+        lines = new ArrayList < String >        ();
+        wholenumbers = new ArrayList< Integer > ();
+        names= new ArrayList<String>            ();
+        index=0;
+    }
+
+    public boolean EvaluateEqualNum(){
+        String s= this.lines.get(index);
+
+        return(s.matches("[A-Z]\\s=\\s\\d+|[A-Z]\\s=\\s\\-\\d+" ));
+    }
+
+    public boolean EvaluateEqualLtr(){
+        String s= this.lines.get(index);
+
+        return(s.matches("[A-Z]\\s=\\s[A-Z]" ));
+    }
+
+    public boolean EvaluateEqualLtrAndNum(){
+        String s= this.lines.get(index);
+
+        return(s.matches(
+                "[A-Z]\\s=\\s[A-Z]\\s\\+\\s\\d+[A-Z]\\s=\\s[A-Z]\\s\\+\\s\\-\\d+" ));
+    }
+
+    public boolean EvaluateEqualNumAndLtr(){
+        String s= this.lines.get(index);
+
+        return(s.matches(
+                "[A-Z]\\s=\\s\\d+\\s\\+\\s[A-Z]|[A-Z]\\s=\\s\\-\\d+\\s\\+\\s[A-Z]" ));
+    }
+    public boolean EvaluateEqualNumAndNum(String s0){
+
+
+        return(s0.matches(
+                "[A-Z]\\s=\\s(\\d+|\\-\\d+)\\s\\+\\s(\\d+|\\-\\d+)"));
+    }
+
+    public boolean EvaluateOutput(){
+        return (this.lines.get( index ).length()==0);
+    }
+
+    public void GetLtrToLtr(String s0){
+
+        String s1="";s1+=s0.charAt(0);
+        String s2="";s2+=s0.charAt(4);
+        int i=0,index=0,num=0;
+        for(i=0;i<this.names.size();++i){
+            if(this.names.get(i).matches(s2)){
+                index=i;
+                num=this.wholenumbers.get(i);
+            }
+        }
+        for(i=0;i<this.names.size();++i) {
+            if (this.names.get( i ) .matches(s1)) {
+                this.wholenumbers.set( i, num );
+            }
+        }
+    }
+    public void GetLtrToNum(String s0){
+
+        String s1="";s1+=s0.charAt(0);
+        String s2="";
+        for(int j=4;j<s0.length();++j) s2+=s0.charAt(j);
+        int staticnum= Integer.parseInt(s2);
+        int i=0,index=0,num=0;
+
+        boolean b=false;
+        for(i=0;i<this.names.size();++i) {
+
+            if (s1.matches( this.names.get(i) )) {
+                this.wholenumbers.set( i, staticnum );
+                b=true;
+                System.out.println("Got it!");
+            }
+        }
+        if(b==false){
+            this.names.add(s1);
+            this.wholenumbers.add(staticnum);
+        }
+    }
+
+    public void GetLtrToLtrNum(String s0){
+
+        String s1="";s1+=s0.charAt(0);
+        String s2="";s2+=s0.charAt(4);
+        String s3="";
+        for(int j=8;j<s0.length();++j) s3+=s0.charAt(j);
+        int staticnum= Integer.parseInt(s3);
+
+        int i=0,index=0,num=0;
+
+        boolean b=false;
+        for(i=0;i<this.names.size();++i) {
+
+            if (s2.matches( this.names.get(i) )) {
+                num=this.wholenumbers.get(i);
+            }
+        }
+        for(i=0;i<this.names.size();++i) {
+
+            if (s1.matches( this.names.get(i) )) {
+                this.wholenumbers.set(i,num+staticnum);
+            }
+        }
+    }
+
+    public void GetLtrToNumNum(String s0) {
+        String s1="";s1 += s0.charAt(0);
+        String stemp="";
+        for(int i=4;i<s0.length();++i)
+            for(int j=i;j<s0.length();++j){
+                stemp +=s0.charAt(j);
+                if(s0.matches( "\\-|\\-\\d+|\\d+")){
+
+                }
+            }
+
+    }
+
+    public void Evaluate(){
+        int i=0,j=0,k=0;
+        for(i=0;i<this.lines.size();++i){
+            this.index=i;
+            if(this.EvaluateEqualLtr())
+            {
+                GetLtrToLtr( this.lines.get(index) );
+            }
+            else if(this.EvaluateEqualNum()){
+
+            }
+        }
+    }
+
+    public void OutputAll(){
+        System.out.println();
+        for(int i=0;i<this.names.size();++i)
+            System.out.println(this.names.get(i)+"  "+this.wholenumbers.get(i));
+    }
+
+    public boolean FindMatch(String s0){
+        System.out.println(
+                s0.matches("[A-Z]\\s\\=\\s"+
+                        "([A-Z]|\\-\\d+|\\d+)"+
+                        "\\s\\+\\s"+"" +
+                        "([A-Z]|\\-\\d+|\\d+)")
+                        || s0.matches( "[A-Z]\\s\\=\\s[A-Z]")
+        );
+        Boolean b=(s0.matches("[A-Z]\\s\\=\\s"+
+                "([A-Z]|\\-\\d+|\\d+)"+
+                "\\s\\+\\s"+"" +
+                "([A-Z]|\\-\\d+|\\d+)")
+                || s0.matches( "[A-Z]\\s\\=\\s[A-Z]"));
+        return b;
+
+    }
+
+    public void OutputMatch(String s0) {
+        if (this.FindMatch( s0 )) {
+            String s1 = "";
+            s1 += s0.charAt( 0 );
+            System.out.print( s1 + " = " );
+            String[] snote = new String[2];
+            snote[0] = "";
+            snote[1] = "";
+            int[][] numnote = new int[2][2];
+            numnote[0][0] = 0;
+            numnote[0][1] = 0;
+            numnote[1][0] = 0;
+            numnote[1][1] = 0;
+            String s = "";
+            int strindex = 0, numindex = 0;
+
+            for (int i = 4; i < s0.length(); ++i) {
+
+                s += s0.charAt( i );
+                String temp = "";
+                temp += s0.charAt( i );
+                if (s0.length()==5) { snote[0]+=s0.charAt(4);  }
+
+
+                else if( temp.matches( "\\s" )) {
+                    else if (s.matches( "\\+\\s" )) {
+                        s = "";
+                        for(int j=i+1;j<s0.length();++j)
+                            s+=s0.charAt(j);
+                        System.out.println("{"+s+ "}");
+                        if (s.matches( "[A-Z]" )) {
+                            String temp2 = "";
+                            temp2 += s.charAt( 0 );
+                            if (strindex == 0) {
+                                snote[0] = temp2;
+                                ++strindex;
+                            } else if (strindex == 1) {
+                                snote[1] = temp2;
+                            }
+                            s = "";
+
+                        } else if (s.matches( "\\d+\\s|\\-\\d+" )) {
+                            String temp2 = "";
+
+                            int num = Integer.parseInt( s );
+                            if (numindex == 0) {
+                                numnote[0][0] = num;
+                                numnote[0][1] = 1;
+                                ++numindex;
+                            } else if (numindex == 1) {
+                                numnote[1][0] = num;
+                                numnote[0][1] = 1;
+                            }
+                            s = "";
+                        }
+                    
+                    }
+                    else if (s.matches( "\\+\\s" )) {
+                        s = "";
+                        for(int j=i+1;j<s0.length();++j)
+                            s+=s0.charAt(j);
+                        System.out.println("{"+s+ "}");
+                        if (s.matches( "[A-Z]" )) {
+                            String temp2 = "";
+                            temp2 += s.charAt( 0 );
+                            if (strindex == 0) {
+                                snote[0] = temp2;
+                                ++strindex;
+                            } else if (strindex == 1) {
+                                snote[1] = temp2;
+                            }
+                            s = "";
+
+                        } else if (s.matches( "\\d+\\s|\\-\\d+" )) {
+                            String temp2 = "";
+
+                            int num = Integer.parseInt( s );
+                            if (numindex == 0) {
+                                numnote[0][0] = num;
+                                numnote[0][1] = 1;
+                                ++numindex;
+                            } else if (numindex == 1) {
+                                numnote[1][0] = num;
+                                numnote[0][1] = 1;
+                            }
+                            s = "";
+                        }
+                    }
+                }
+
+
+            }
+            System.out.println("String: ");
+            System.out.println(snote[0]+"  "+snote[1]);
+            System.out.println("number: ");
+            System.out.println(numnote[0][0]+"  "+numnote[1][0]);
+
+        }
+
+    }
+
+
+
+
+    public static void main (String args[]){
+
+        MiniInterpreter m= new MiniInterpreter();
+        //Do some tests for the GetLtrToNum();
+
+        m.GetLtrToNum( "A = 113" );
+        m.OutputAll();
+
+        m.GetLtrToNum( "A = 124" );
+        m.OutputAll();
+
+        m.GetLtrToNum( "A = -124" );
+
+        m.OutputAll();
+        m.GetLtrToNum( "B = 124" );
+
+        m.OutputAll();
+        m.GetLtrToNum( "A = 129" );
+
+        m.OutputAll();
+        m.GetLtrToNum( "C = 129" );
+
+        m.OutputAll();
+        m.GetLtrToNum( "C = 139" );
+
+        m.OutputAll();
+        m.GetLtrToLtr( "A = B" );
+
+        m.OutputAll();
+        m.GetLtrToLtr( "C = B" );
+
+        m.OutputAll();
+        m.GetLtrToLtrNum( "C = B + 123" );
+
+        m.OutputAll();
+        m.GetLtrToLtrNum( "C = B + -123" );
+
+        m.OutputAll();
+        System.out.println(m.EvaluateEqualNumAndNum("C = -23 + -445"));
+        System.out.println(m.EvaluateEqualNumAndNum("C = -23 + A"));
+        System.out.println(m.EvaluateEqualNumAndNum("C = 23 + -445"));
+        System.out.println(m.EvaluateEqualNumAndNum("C = B + -445"));
+        System.out.println(m.EvaluateEqualNumAndNum("C = 0313 + 245"));
+        System.out.println("start the test");
+        m.FindMatch( "C = B" );
+        m.FindMatch( "C = B + D" );
+        m.FindMatch( "C = B + -123" );
+        m.FindMatch( "C = -203 + -123" );
+        m.FindMatch( "C = H" );
+        m.FindMatch( "C = -002020202 + C" );
+        m.FindMatch( "C = -002020202 + CD" );
+        m.FindMatch( "C = -002020202 + ." );
+        m.FindMatch( "D=1234 + 1234455667" );
+        System.out.println(Integer.parseInt("0010203"));
+        System.out.println(Integer.parseInt("-0010203"));
+        System.out.println(Integer.parseInt("00000010203"));
+        System.out.println(Integer.parseInt("-00000"));
+        System.out.println(Integer.parseInt("00000"));
+        m.OutputMatch("A = B + C");
+        m.OutputMatch("A = 1492 + -2039");
+        m.OutputMatch("A = 0012 + -039");
+        m.OutputMatch("A = -0012 + 039");
+
+
+    }
+
+}
